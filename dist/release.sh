025824d2f42c0884e8f7acf31a515a8a457ede5b
Arnstein Ressem <aressem@yahoo-inc.com>
2017-06-12 13:31:54 +0200

    Script change.

--- dist/release.sh
+++ dist/release.sh
@@ -1,12 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
 
 readonly VERSION=$1
 
-git push origin :rpmbuild || true
-git branch -D rpmbuild || true 
-git checkout -b rpmbuild
+git checkout master
+git push origin :rpmbuild &> /dev/null || true
+git branch -D rpmbuild &> /dev/null || true 
+git checkout -b rpmbuild $VERSION
 git mv dist/simple.spec .
 tito init
 tito tag --use-version=$VERSION --no-auto-changelog
 git push -u origin --follow-tags rpmbuild
+git checkout master
 
